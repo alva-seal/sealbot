67d6df4bfc6bd669e40f043a7d1bbfb19b6c348c
fabi <github@ffabi.com>
2020-11-06 06:13:32 +0000

    save changes

--- code/bot.py
+++ code/bot.py
@@ -1,5 +1,5 @@
-import discord
 import config
+import discord
 
 
 class MyBot(discord.Client):
@@ -9,25 +9,24 @@ class MyBot(discord.Client):
     async def on_message(self, message):
         if (message.content == "seal"):
             print('Message from {0.author}: {0.content}'.format(message))
-            await
-            message.channel.send("otter")
+            await message.channel.send("otter")
 
         if (message.content == "whoami"):
             print('Message from {0.author}: {0.content}'.format(message))
-            await
-            message.channel.send('You are {0.author}'.format(message))
+            await message.channel.send('You are {0.author}'.format(message))
             # breakpoint()
-            await
-            message.channel.send("test")
+            await message.channel.send("test")
 
         if (message.content == "Voice"):
             print('Message from {0.author}: {0.content}'.format(message))
             channel = discord.utils.get(message.guild.channels, name="music")
-            voicechannel = await
-            channel.connect()
+            voicechannel = await channel.connect()
             # voicechannel.play(discord.PCMAudio('test.wav'))
             # breakpoint()
             voicechannel.play(discord.FFmpegPCMAudio('test.mp3'))
+        if (messsage.content == "Stop"):
+            print('test')
+
 
 
 client = MyBot()
